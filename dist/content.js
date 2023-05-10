52f7a637e1fe33600ef11917dc0f0a1348bbd045
Jack Allen <jackallensemail+git@gmail.com>
2023-05-10 22:54:49 +0100

    resources

--- dist/content.js
+++ dist/content.js
@@ -8,9 +8,9 @@ var __webpack_exports__ = {};
 console.log("Hello, world!");
 var root = document.createElement("div");
 root.id = "root";
-root.innerHTML = "<span>I'm here!</span>";
+root.innerHTML = "<span>🍆</span>";
 document.body.appendChild(root);
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udGVudC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBYTtBQUNiO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zaGFyZS1zZXNzaW9uLy4vc3JjL2NvbnRlbnQudHMiXSwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XG5jb25zb2xlLmxvZyhcIkhlbGxvLCB3b3JsZCFcIik7XG52YXIgcm9vdCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoXCJkaXZcIik7XG5yb290LmlkID0gXCJyb290XCI7XG5yb290LmlubmVySFRNTCA9IFwiPHNwYW4+SSdtIGhlcmUhPC9zcGFuPlwiO1xuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChyb290KTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udGVudC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBYTtBQUNiO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zaGFyZS1zZXNzaW9uLy4vc3JjL2NvbnRlbnQudHMiXSwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XG5jb25zb2xlLmxvZyhcIkhlbGxvLCB3b3JsZCFcIik7XG52YXIgcm9vdCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoXCJkaXZcIik7XG5yb290LmlkID0gXCJyb290XCI7XG5yb290LmlubmVySFRNTCA9IFwiPHNwYW4+8J+Nhjwvc3Bhbj5cIjtcbmRvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQocm9vdCk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
