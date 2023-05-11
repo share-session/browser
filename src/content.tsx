74c81d29eb47709b6ebd767e828889c67b7cd874
Jack Allen <jackallensemail+git@gmail.com>
2023-05-11 10:52:40 +0100

    tailwind

--- src/content.tsx
+++ src/content.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import './styles.css';
+import "./styles.css";
 
-const root = document.createElement("div")
+const root = document.createElement("div");
 root.id = "share-session-root";
 document.body.appendChild(root);
 
 function Root() {
-  return (
-    <div className="share-session">
-      🍆
-    </div>
-  )
+  return <div className="share-session">🍆</div>;
 }
 
 createRoot(root).render(<Root />);
-
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
             ,
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
             },
         ]
     }
